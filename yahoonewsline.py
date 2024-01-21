25c2c9a0b7917b2267d6645f6eb974194816216a
poteto <naonyan1135@gmail.com>
2024-01-21 19:42:30 +0900

    Add Brave Search functionality

--- yahoonewsline.py
+++ yahoonewsline.py
@@ -15,8 +15,10 @@ app = Flask(__name__)
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 
+
 configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -31,10 +33,12 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        app.logger.info(
+            "Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
@@ -53,71 +57,111 @@ def handle_message(event):
         elif user_message.startswith('@google'):
             search_word = user_message[len('@google'):].strip()
             search_and_send_google_news(search_word, event)
+        # '@brave'という単語で始まるメッセージを処理
+        elif user_message.startswith('@brave'):
+            search_word = user_message[len('@brave'):].strip()
+            search_and_send_brave_search(search_word, event)
+
     # 個人LINEの場合
     elif source_type == 'user':
         # '@google'という単語で始まるメッセージを処理
         if user_message.startswith('@google'):
             search_word = user_message[len('@google'):].strip()
             search_and_send_google_news(search_word, event)
-        # '@google'がない場合は Yahoo News を検索
+            # '@brave'という単語で始まるメッセージを処理
+        elif user_message.startswith('@brave'):
+            search_word = user_message[len('@brave'):].strip()
+            search_and_send_brave_search(search_word, event)
+
+        # '@googleと@braveがない場合は Yahoo News を検索
         else:
             search_word = user_message
             search_and_send_yahoo_news(search_word, event)
 
 # Yahoo Newsの検索とメッセージ送信を行う関数
-def search_and_send_yahoo_news(search_word, event):
-    # 検索語をURLエンコード
-    encoded_search_word = urllib.parse.quote(search_word)
 
+
+def search_and_send_yahoo_news(search_word, event):
     # Yahoo Newsの検索URL
-    yahoo_news_search_url = f'https://news.yahoo.co.jp/search?p={encoded_search_word}&ei=utf-8'
+    yahoo_news_search_url = f'https://news.yahoo.co.jp/search?p={
+        urllib.parse.quote(search_word)}&ei=utf-8'
 
     # スクレイピングしてニュースの見出しとURLを取得
     response = requests.get(yahoo_news_search_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Yahoo NewsのHTML構造に基づいて適切なセレクタを指定
     news_links = soup.find_all('a', class_='newsFeed_item_link')
 
     # LINEに送信するメッセージを作成
-    messages = ["Yahooニュースが見つかりました。\n以下のニュースを送信します。"]
-    for link in news_links[:5]:  # 最初の5件のみ処理
-        title = link.find('div', class_='newsFeed_item_title').get_text(strip=True)
-        url = link['href']
-        messages.append(f"{title}\n{url}")
+    if news_links:
+        messages = ["Yahooニュースが見つかりました。\n以下のニュースを送信します。"]
+        for link in news_links[:4]:  # 最初の4件のみ処理
+            title = link.find(
+                'div', class_='newsFeed_item_title').get_text(strip=True)
+            url = link['href']
+            messages.append(f"{title}\n{url}")
+    else:
+        messages = ["Yahooニュースが見つかりませんでした。"]
 
     # LINE Botを通じてメッセージを送信
     send_messages_to_line(messages, event)
 
+
 # Google Newsの検索とメッセージ送信を行う関数
 def search_and_send_google_news(search_word, event):
-    # 検索語をURLエンコード
-    encoded_search_word = urllib.parse.quote(search_word)
-
     # Google Newsの検索URL
-    google_news_search_url = f'https://news.google.com/search?q={encoded_search_word}&hl=ja&gl=JP&ceid=JP%3Aja'
+    google_news_search_url = f'https://news.google.com/search?q={
+        urllib.parse.quote(search_word)}&hl=ja&gl=JP&ceid=JP:ja'
 
     # スクレイピングしてニュースの見出しとURLを取得
     response = requests.get(google_news_search_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # 提供されたHTML構造に基づいて適切なセレクタを指定
     news_items = soup.select('a.WwrzSb')
 
     # LINEに送信するメッセージを作成
-    messages = ["Googleニュースが見つかりました。\n以下のニュースを送信します。"]
-    for item in news_items[:5]:  # 最初の5件のみ処理
-        # Google Newsのリンクは相対パスで提供されるため、絶対パスに変換
-        url = urllib.parse.urljoin('https://news.google.com/', item['href'])
-        # タイトルは親要素から取得
-        title = item.text
-        messages.append(f"{title}\n{url}")
+    if news_items:
+        messages = ["Googleニュースが見つかりました。\n以下のニュースを送信します。"]
+        for item in news_items[:4]:  # 最初の4件のみ処理
+            url = urllib.parse.urljoin(
+                'https://news.google.com/', item['href'])
+            title = item.text
+            messages.append(f"{url}")
+    else:
+        messages = ["Googleニュースが見つかりませんでした。"]
 
     # LINE Botを通じてメッセージを送信
     send_messages_to_line(messages, event)
-    
+
+
+def search_and_send_brave_search(search_word, event):
+    # Brave SearchのURLを作成
+    brave_search_url = f'https://search.brave.com/search?q={search_word}'
+
+    # リクエストを送信し、レスポンスを取得
+    response = requests.get(brave_search_url)
+
+    # HTMLを解析
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # 結果の一部を抽出
+    results = soup.find_all('a', class_='h svelte-1dihpoi')
+
+    # LINEに送信するメッセージを作成
+    if results:
+        messages = ["Brave Searchの結果を送信します。"]
+        for result in results[:4]:  # 最初の4件のみ処理
+            title = result.find(
+                'div', class_='url svelte-1dihpoi heading-serpresult').text
+            link = result['href']
+            messages.append(f"{title}\n{link}")
+    else:
+        messages = ["Brave Searchの結果が見つかりませんでした。"]
+
+    # LINE Botを通じてメッセージを送信
+    send_messages_to_line(messages, event)
+
 
 # LINEにメッセージを送信する関数
+
 def send_messages_to_line(messages, event):
     # メッセージが5つを超える場合は最初の5つのみを使用
     messages = messages[:5]
@@ -129,7 +173,8 @@ def send_messages_to_line(messages, event):
                 response = line_bot_api.reply_message_with_http_info(
                     ReplyMessageRequest(
                         reply_token=event.reply_token,
-                        messages=[TextMessage(text=message) for message in messages]
+                        messages=[TextMessage(text=message)
+                                  for message in messages]
                     )
                 )
                 print(response)  # 応答内容をログに出力
@@ -149,6 +194,7 @@ def send_messages_to_line(messages, event):
                 print(response)  # 応答内容をログに出力
             except Exception as e:
                 print(f"Exception when calling LINE Messaging API: {e}")
+
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=8000)
